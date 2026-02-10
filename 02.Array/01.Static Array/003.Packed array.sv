9f491a991566dd33d1bb264fc4dd1ef7a48e51e7
Sibiraj M <sibiraj.github@gmail.com>
2026-02-10 19:02:34 +0530

    Create 003.Packed array.sv

---
+++ 02.Array/01.Static Array/003.Packed array.sv
@@ -0,0 +1,20 @@
+module packed_array_single;
+  bit [7:0] arr;
+  initial begin
+    arr=8'hff;
+    foreach(arr[i])begin
+    $display("Arr[%0d]=%b",i,arr[i]);
+    end
+  end
+endmodule
+
+/*OUTPUT:
+Arr[7]=1
+Arr[6]=1
+Arr[5]=1
+Arr[4]=1
+Arr[3]=1
+Arr[2]=1
+Arr[1]=1
+Arr[0]=1
+*/
