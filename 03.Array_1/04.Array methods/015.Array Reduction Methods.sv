b18cdccaad408084dd65a4b1a927d62ca8007a78
Sibiraj M <sibiraj.github@gmail.com>
2026-02-12 22:42:02 +0530

    Create 015.Array Reduction Methods.sv

---
+++ 03.Array_1/04.Array methods/015.Array Reduction Methods.sv
@@ -0,0 +1,33 @@
+module arr_rec;
+  int arr[5];
+  initial begin
+    foreach(arr[i])
+      arr[i]=$random % 100;
+    $display("value:%p ",arr);
+    $display("sum    :%p",arr.sum());
+    $display("value:%p ",arr);
+    $display("proiduct    :%p",arr.product());
+    $display("value:%p ",arr);
+    $display("and     :%p",arr.and());
+    $display("value:%p ",arr);
+    $display("OR    :%p",arr.or());
+    $display("value:%p ",arr);
+    $display("XOR     :%p",arr.xor());
+  end
+endmodule
+
+/*OUTPUT:
+xcelium> run
+value:'{48, -99, -39, -9, 57} 
+sum    :-42
+value:'{48, -99, -39, -9, 57} 
+proiduct    :-95073264
+value:'{48, -99, -39, -9, 57} 
+and     :16
+value:'{48, -99, -39, -9, 57} 
+OR    :-1
+value:'{48, -99, -39, -9, 57} 
+XOR     :-70
+xmsim: *W,RNQUIE: Simulation is complete.
+*/
+    
